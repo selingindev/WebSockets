fd2f67e7e78713f71bf3b08eec0da8c3970369be
Arthur Selingin <arthur_santos@outlook.com.br>
2025-04-08 22:02:33 -0300

    anotações do fluxo do WebSocket frame

--- src/WebSocketServer.java
+++ src/WebSocketServer.java
@@ -29,6 +29,7 @@ public class WebSocketServer {
             String data = request.toString();
             System.out.println("Handshake recebido:\n" + data);
 
+            //passando uma requisição http especial para mudar de http para ws
             Matcher get = Pattern.compile("^GET").matcher(data);
             if (get.find()) {
                 Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
@@ -51,24 +52,35 @@ public class WebSocketServer {
 
             // Agora, receber mensagens WebSocket
             while (true) {
+
+                //recebendo os byte com bit reservados e custmizações e tipo de dados 
                 int b1 = iS.read();
+               
+                //pegando o length em byte
                 int b2 = iS.read();
 
                 if (b1 == -1 || b2 == -1) {
                     System.out.println("Cliente desconectado.");
                     break;
                 }
-
+                
+                //final frame bit, 1 = ultimo bit para descobrir o ultimo valor
                 boolean fin = (b1 & 0x80) != 0;
-                int opcode = b1 & 0x0F;
-                boolean masked = (b2 & 0x80) != 0;
-                int payloadLength = b2 & 0x7F;
 
+                //peganado o tipo de dados = 129 então texto, 0x07 apenas pega os 4 primeiro bits da direita, exlcuindo da esquerda
+                int opcode = b1 & 0x0F;
+
+                //mascara obrigatoria do cliente, 1bit
+                boolean masked = (b2 & 0x80) != 0;
+
+                //pegando o tamanho de bytes presentes
+                int payloadLength = b2 & 0x7F;
                 if (!masked) {
                     System.out.println("Frame não mascarado! (cliente deve sempre mascarar)");
                     break;
                 }
 
+                //se o payload igual 126 espera receber mais dois bytes para pegar o tamanho real
                 if (payloadLength == 126) {
                     payloadLength = (iS.read() << 8) | iS.read();
                 } else if (payloadLength == 127) {
@@ -76,17 +88,21 @@ public class WebSocketServer {
                     break;
                 }
 
+                //preparando para receber os quatro bytes de maskingkey do cliente
                 byte[] maskingKey = new byte[4];
                 iS.read(maskingKey, 0, 4);
 
+                //recebendo unicamente os 11bytes(pegando pelo payloadLentgh) do b2
                 byte[] encodedData = new byte[payloadLength];
                 iS.read(encodedData, 0, payloadLength);
 
+                //criando array de bytes para destribuir a masking de quatro em quatro
                 byte[] decodedData = new byte[payloadLength];
                 for (int i = 0; i < payloadLength; i++) {
                     decodedData[i] = (byte) (encodedData[i] ^ maskingKey[i % 4]);
                 }
 
+                //Retorna uma string com cada byte correspondente de cada letra formando a palavra
                 String mensagem = new String(decodedData, "UTF-8");
                 System.out.println("Mensagem recebida: " + mensagem);
 
